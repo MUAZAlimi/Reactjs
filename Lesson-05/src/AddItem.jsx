5cc6de1feac7a5da97fa2028e51876d6d0baf0e5
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-10-26 16:00:14 +0100

    React

--- Lesson-05/src/AddItem.jsx
+++ Lesson-05/src/AddItem.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { FaPlus } from 'react-icons/fa'
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+import { useRef } from "react";
 
-const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+    const inputRef = useRef()
+
   return (
-    <form className='addForm' onSubmit={handleSubmit}>
-        <label htmlFor="addItem">Add Item</label>
-        <input type="text" id='addItem' placeholder='add item' required value={newItem} onChange={(e) => setNewItem(e.target.value)} />
-        <button type='submit' aria-label='Add Item'><FaPlus/></button>
+    <form className="addForm" onSubmit={handleSubmit}>
+      <label htmlFor="addItem">Add Item</label>
+      <input
+        type="text"
+        id="addItem"
+        autoFocus
+        ref={inputRef}
+        placeholder="add item"
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button type="submit" aria-label="Add Item">
+        <FaPlus />
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default AddItem
+export default AddItem;
