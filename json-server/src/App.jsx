8b1c868c25ff867aaf56fe9b8178d884cfbacfda
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-10-30 10:48:08 +0100

    Reactjs

---
+++ json-server/src/App.jsx
@@ -0,0 +1,69 @@
+import Content from './Content'
+import Footer from './Footer'
+import Header from './Header'
+import AddItem from './AddItem'
+import { useState, useEffect } from 'react'
+import SearchItem from './SearchItem'
+import { render } from '@testing-library/react'
+
+
+function App() {
+  const [newItem, setNewItem] = useState('')
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('Shoppinglist')))
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem('Shoppinglist', JSON.stringify(items))
+  }, [items]);
+
+    // const setAndSaveItems = (listItems) => {
+    //   setItems(listItems);
+    // }
+
+    const addItem = (item) => {
+      const id = items.length ? items[items.length-1].id + 1 : 1;
+      const myNewItem = {id, checked:false, item}
+      const listItems = [...items, myNewItem]
+      setItems(listItems)
+    }
+
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id?{...item, checked: !item.checked} : item)
+        setItems(listItems)
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id)
+        setItems(listItems)
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if(!newItem) return;
+      addItem(newItem)
+      setNewItem('')
+    }
+
+  return (
+    <div className='App'>
+        <Header title="Cohort 3 List"/>                        {/* Custom Element */}
+        <AddItem 
+          newItem={newItem} 
+          setNewItem={setNewItem} 
+          handleSubmit={handleSubmit} 
+        />
+        <SearchItem 
+          search={search} 
+          setSearch={setSearch} 
+        />
+        <Content  
+          items={items.filter(item =>((item.item).toLowerCase()).includes(search.toLocaleLowerCase()))} 
+          handleCheck={handleCheck} 
+          handleDelete={handleDelete}
+        />                       {/* Custom Element */}
+        <Footer length={items.length}/>                         {/* Custom Element */}
+    </div>
+  )
+}
+
+export default App
