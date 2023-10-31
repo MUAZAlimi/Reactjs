e2e601cde7d069f8d6091acdb25aa4d75a9d4b50
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-10-31 15:19:40 +0100

    reactjs

--- react-router-dom-project/src/App.js
+++ react-router-dom-project/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Routes, Route, useNegative } from 'react-router-dom'
+import Header from './Header'
+import Nav from './Nav'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import Missing from './Missing'
+import About from './About'
+import Footer from './Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <Header />
+        <Nav />
+        <Routes>
+          <Route path='/' element={ <Home />} />
+          <Route path='/post' element={ <NewPost />} />
+          <Route path='/post/:id' element={  <PostPage />} />
+
+          <Route path='/about' element={   <About />} />
+          <Route path='' element={   <Missing />} />
+        </Routes>
+        <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
