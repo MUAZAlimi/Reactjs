15d331a8d9ea5e6359975b3c0a4fd3c13bf6b363
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-03 12:42:05 +0100

    Reactjs

--- react-router-dom-project/src/App.js
+++ react-router-dom-project/src/App.js
@@ -68,23 +68,23 @@ const App = () => {
     setSearchResult(filterResult.reverse());
   }, [posts, search]);
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await Api.get("/posts");
-        setPosts(response.data);
-      } catch (err) {
-        if (err.message) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    };
-    fetchPost();
-  }, []);
+  // useEffect(() => {
+  //   const fetchPost = async () => {
+  //     try {
+  //       const response = await Api.get("/posts");
+  //       setPosts(response.data);
+  //     } catch (err) {
+  //       if (err.message) {
+  //         console.log(err.response.data);
+  //         console.log(err.response.status);
+  //         console.log(err.response.headers);
+  //       } else {
+  //         console.log(`Error: ${err.message}`);
+  //       }
+  //     }
+  //   };
+  //   fetchPost();
+  // }, []);
 
   const handleEdit = async (id) => {
     const date = format(new Date(), "MMMM dd, yyyy pp");
