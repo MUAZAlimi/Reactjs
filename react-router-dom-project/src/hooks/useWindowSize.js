a6a96cc3ad0db614a09bb9804b8060f138783a3d
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-03 10:11:46 +0100

    Reactjs

--- react-router-dom-project/src/hooks/useWindowSize.js
+++ react-router-dom-project/src/hooks/useWindowSize.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react' 
 
 const useWindowSize = () => {
-    const [windowSize, setWindowSize] = useState({ width: undefined,
+    const [windowSize, setWindowSize] = useState({ 
+        width: undefined,
         height: undefined
-
-    })
-           
+    })      
 }
 
 export default useWindowSize
