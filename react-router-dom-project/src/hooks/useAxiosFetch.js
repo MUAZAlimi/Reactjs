936d704aebaf6d0abf11c11658c4f90f007e93cd
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-03 11:26:15 +0100

    Reactjs

---
+++ react-router-dom-project/src/hooks/useAxiosFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react' ;
+import axios from "axios";
+
+const useAxiosFetch = (dataUrl) => {
+    const [data, setData] = useState([])
+    const [fetchError, setFetchError] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+
+    
+ return (
+    <>
+
+    </>
+ )
+}
+
+export default useAxiosFetch
