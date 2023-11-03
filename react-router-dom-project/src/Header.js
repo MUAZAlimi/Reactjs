21cbead001e1f7c36da55d8bbaf341c298385886
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-03 11:07:02 +0100

    reactjs

--- react-router-dom-project/src/Header.js
+++ react-router-dom-project/src/Header.js
@@ -5,7 +5,10 @@ const Hearder = ({ title, width }) => {
     <header className='Header'>
 
     <h1>{title}</h1>
-      
+      {width <768 ? <FaMobileAlt/>
+        : width <992 ? <FaTabletAlt />
+        : <FaLaptop/>  
+    }
     </header>
   )
 }
