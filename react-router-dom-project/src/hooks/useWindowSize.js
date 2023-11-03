0028596d4d17174d6d7e101effa29228c3e89cd5
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-03 10:22:43 +0100

    react

--- react-router-dom-project/src/hooks/useWindowSize.js
+++ react-router-dom-project/src/hooks/useWindowSize.js
@@ -17,7 +17,7 @@ const useWindowSize = () => {
       window.addEventListener("resize", handleResize)
       return () => window.addEventListener("resize", handleResize)
     }, [])
-    
+    return windowSize
 }
 
 export default useWindowSize
