dfee066a783b275b6a47c0c107bf33a4f70be28c
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-06 11:02:37 +0100

    Reactjs

--- data-context/src/App.js
+++ data-context/src/App.js
@@ -90,7 +90,7 @@ const App = () => {
       <Route
         path=""
         element={
-          <HomeLayout search={search} setSearch={setSearch} />
+          <HomeLayout/>
         }
       >
         <Route index element={<Home 
--- data-context/src/context/DataContext.js
+++ data-context/src/context/DataContext.js
@@ -82,7 +82,7 @@ export const DataProvider = ({children}) => {
       };
     return (
         <DataContext.Provider value={{
-            width,
+            width,search,setSearch
         }}>
             {children}
         </DataContext.Provider> 
