53154326ea96faa89d4b9d6529d4db0518b49148
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-06 09:52:50 +0100

    Reactjs

--- data-context/src/context/DataContext.js
+++ data-context/src/context/DataContext.js
@@ -1,12 +1,5 @@
 import { createContext, useEffect, useState } from 'react'
 
 const DataContext = createContext({}) 
-// => {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
 
-export default DataContext
+export 
