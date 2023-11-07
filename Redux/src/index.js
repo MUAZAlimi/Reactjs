a9f6dd47f0505e98aa43956257f35e1f34815d81
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-07 11:18:50 +0100

    Reacjs

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { StoreProvider } from "easy-peasy";
+import store from "./store";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
