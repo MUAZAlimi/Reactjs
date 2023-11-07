bed69256b2d72dfd4d03ba311d364ebe084bf54d
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-07 14:28:50 +0100

    Reactjs

--- Redux/src/EditPost.jsx
+++ Redux/src/EditPost.jsx
@@ -1,33 +1,16 @@
 import React, { useEffect, useContext } from 'react'
 import { Link, Navigate, useParams } from 'react-router-dom'
-import DataContext from './context/DataContext';
 import { format } from 'date-fns';
-import  Api  from "./Api/Posts";
-import { useState } from 'react';
 
 const EditPost = () => {
-    const [editTitle, setEditTitle] = useState("");
-    const [editBody, setEditBody] = useState("");
     const navigate = Navigate()
-
-    const { posts, setPosts } = useContext(DataContext)
     const { id } = useParams();
-    const post = posts.find( post => (post.id).toString() === id)
 
     const handleEdit = async (id) => {
         const date = format(new Date(), "MMMM dd, yyyy pp");
         const updatedPost = { id, title: editTitle, date, body: editBody };
-        try {
-          const response = await Api.put(`/posts/${id}`, updatedPost);
-          setPosts(
-            posts.map( post => (post.id === id ? { ...response.data } : post))
-          );
-          setEditTitle("");
-          setEditBody("");
+       
           navigate("/");
-        } catch (err) {
-          console.log(`Error: ${err.message}`);
-        }
       };
 
     useEffect(() => {
