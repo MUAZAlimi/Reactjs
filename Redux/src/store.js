399054e2fe1203e52bc26b85cbaf822fc05a831c
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-07 10:53:11 +0100

    Reactjs

--- Redux/src/store.js
+++ Redux/src/store.js
@@ -1,40 +1,40 @@
-import { createStore, action, thunk, computed } from "easy-peasy"; 
+import { createStore, action, thunk, computed } from "easy-peasy";
 import Api from "./Api/Posts";
 
 export default createStore({
-    posts: [],
-    setPosts: action((state, payLoad) => {
-        state.posts = payLoad;
-    }),
-    postTitle: "",
-    setPostTitle: action((state, payload) => {
-        state.postTitle = payload;
-    }),
-    postBody: "",
-    setPostBody: action((state, payload) => {
-        state.postBody = payload;
-    }),
-    editTitle: "",
-    setEditTitle: action((state, payload) => {
-        state.editTitle = payload;
-    }),
-    editBody: "",
-    setEditBody: action((state, payload) => {
-        state.editBody = payload;
-    }),
-    search: "",
-    setSearch: action((state, payload) => {
-        state.search = payload;
-    }),
-    searchResults: [],
-    setSearchResults: action((state, payload) => {
-        state.searchResults = payload;
-    }),
-    setPostCount: computed((state) => state.posts.length),
-    setPostId: computed((id) => {
-        return state.posts.find((post) => post.id.toString() === id);
-    }),
-    savePost: thunk(async (actions, newPost, helpers) => {
-        const {posts} = helpers.getState()
-    })
-})
+  posts: [],
+  setPosts: action((state, payLoad) => {
+    state.posts = payLoad;
+  }),
+  postTitle: "",
+  setPostTitle: action((state, payload) => {
+    state.postTitle = payload;
+  }),
+  postBody: "",
+  setPostBody: action((state, payload) => {
+    state.postBody = payload;
+  }),
+  editTitle: "",
+  setEditTitle: action((state, payload) => {
+    state.editTitle = payload;
+  }),
+  editBody: "",
+  setEditBody: action((state, payload) => {
+    state.editBody = payload;
+  }),
+  search: "",
+  setSearch: action((state, payload) => {
+    state.search = payload;
+  }),
+  searchResults: [],
+  setSearchResults: action((state, payload) => {
+    state.searchResults = payload;
+  }),
+  setPostCount: computed((state) => state.posts.length),
+  setPostById: computed((id) => {
+    return state.posts.find((post) => post.id.toString() === id);
+  }),
+  savePost: thunk(async (actions, newPost, helpers) => {
+    const { posts } = helpers.getState();
+  }),
+});
