41b136be1739e577f5914d937049e6f97963a1d0
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-07 10:23:03 +0100

    Reactjs

--- Redux/src/store.js
+++ Redux/src/store.js
@@ -8,6 +8,26 @@ export default createStore({
     }),
     postTitle: "",
     setPostTitle: action((state, payload) => {
-        state.post = payload;
-    })
+        state.postTitle = payload;
+    }),
+    postBody: "",
+    setPostBody: action((state, payload) => {
+        state.postBody = payload;
+    }),
+    editTitle: "",
+    setEditTitle: action((state, payload) => {
+        state.editTitle = payload;
+    }),
+    editBody: "",
+    setEditBody: action((state, payload) => {
+        state.editBody = payload;
+    }),
+    search: "",
+    setSearch: action((state, payload) => {
+        state.search = payload;
+    }),
+    searchResults: [],
+    setSearchResults: action((state, payload) => {
+        state.searchResults = payload;
+    }),
 })
