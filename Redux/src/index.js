e5b02310db37b350c9c1f3572a8ca2c5843a0439
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-07 11:21:42 +0100

    Reactjs

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -9,10 +9,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <StoreProvider store={store}>
     <Router>
       <Routes>
         <Route  path="/*" element={<App />}/>
       </Routes>
     </Router>
+    </StoreProvider>
   </React.StrictMode>
 );
