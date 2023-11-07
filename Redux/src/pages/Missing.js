31f1dd171deb8984648396e6f476816e94002713
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-07 14:58:25 +0100

    Reactjs

--- Redux/src/pages/Missing.js
+++ Redux/src/pages/Missing.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Missing = () => {
   return (
     <div className='red'>
       <h1>404</h1>
       <p>Page not found</p>
+      <Link  to="/">Back to HomePage</Link>
     </div>
   )
 }
