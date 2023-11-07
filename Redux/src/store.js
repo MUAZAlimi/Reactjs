c3065e8ef330405296db0f6686c55c2e47fef709
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-07 10:15:32 +0100

    Reactjs

--- Redux/src/store.js
+++ Redux/src/store.js
@@ -4,6 +4,10 @@ import Api from "./Api/Posts";
 export default createStore({
     posts: [],
     setPosts: action((state, payLoad) => {
-        state.posts = payLoad
+        state.posts = payLoad;
+    }),
+    postTitle: "",
+    setPostTitle: action((state, payload) => {
+        state.post = payload;
     })
 })
